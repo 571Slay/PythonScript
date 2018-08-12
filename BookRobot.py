46b02af9bfe775e7d16aa75a745e92f483e0c4e1
吴烨 <41347481+MercuryYe@users.noreply.github.com>
2018-08-12 17:39:53 +0800

    Update BookRobot.py

--- BookRobot.py
+++ BookRobot.py
@@ -2,7 +2,7 @@
 import os
 import re
 import urllib.request
-from bs4 import BeautifulSoup
+import bs4.BeautifulSoup
 
 #设置代理信息
 headers = ('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36')
